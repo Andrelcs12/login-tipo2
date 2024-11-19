805e74ad13e55f4f5b831e752cfe0480e1ba3134
Andrelcs12 <126403873+Andrelcs12@users.noreply.github.com>
2024-11-19 11:55:21 -0300

    Delete login-complexo directory

--- login-complexo/script.js
+++
@@ -1,9 +0,0 @@
-function cadastro() {
-    let close_login = document.getElementById('left').style.display = 'none';
-    let open_cdt = document.getElementById('page-cdt').style.display = 'block';
-}
-
-function back_login() {
-    let close_cdt = document.getElementById('page-cdt').style.display = 'none';
-    let open_login = document.getElementById('left').style.display = 'block';
-}
